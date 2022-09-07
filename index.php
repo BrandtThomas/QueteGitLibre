658845bc615a88d632b41002711b71313d30e216
EddaariSouad <eddaarisouad@gmail.com>
2022-09-07 11:51:24 +0200

    souad

--- index.php
+++ index.php
@@ -69,6 +69,10 @@
 </div>
 </section>
 
+<footer>
+&copy;St2mflix
+</footer>
+
 
 
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
